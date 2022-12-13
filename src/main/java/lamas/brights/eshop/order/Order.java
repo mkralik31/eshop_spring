677dce7740caf166b708c76e9415c95e6863a1fa
Lubci-Lubci <lubo.han@gmail.com>
2022-12-13 08:51:08 +0100

    order - totalPrice added into model

--- src/main/java/lamas/brights/eshop/order/Order.java
+++ src/main/java/lamas/brights/eshop/order/Order.java
@@ -1,11 +1,12 @@
 package lamas.brights.eshop.order;
 
-import com.sun.istack.NotNull;
+
 import lamas.brights.eshop.product.Product;
 import lamas.brights.eshop.user.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -16,9 +17,11 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotNull
     @Column(name = "order_date")
     private LocalDateTime orderDate = LocalDateTime.now();
+
+    @Column(name = "total_price")
+    private double totalPrice;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
@@ -35,9 +38,10 @@ public class Order {
     public Order() {
     }
 
-    public Order(long id, LocalDateTime orderDate, User user, List<Product> product) {
+    public Order(long id, LocalDateTime orderDate, double totalPrice, User user, List<Product> product) {
         this.id = id;
         this.orderDate = orderDate;
+        this.totalPrice = totalPrice;
         this.user = user;
         this.product = product;
     }
@@ -56,6 +60,14 @@ public class Order {
 
     public void setOrderDate(LocalDateTime orderDate) {
         this.orderDate = orderDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
     }
 
     public User getUser() {
@@ -79,6 +91,7 @@ public class Order {
         return "Order{" +
                 "id=" + id +
                 ", orderDate=" + orderDate +
+                ", totalPrice=" + totalPrice +
                 ", user=" + user +
                 ", product=" + product +
                 '}';
