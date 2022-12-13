d7a97e1ab057fd7836c17e199b1d3623f00cb7d9
Lubci-Lubci <lubo.han@gmail.com>
2022-12-13 10:49:36 +0100

    category rest controller - get all categories

---
+++ src/main/java/lamas/brights/eshop/category/CategoryController.java
@@ -0,0 +1,52 @@
+package lamas.brights.eshop.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryController categoryController){
+        this.categoryService = categoryService;
+    }
+
+
+    @GetMapping("/products/category")
+    public ResponseEntity<List<Locale.Category>> getAllCategories(@RequestParam(required = false) String name) {
+        try {
+            List<Category> category = new ArrayList<>();
+
+            if (name == null){
+                category = categoryService.getAllCategories();
+            } else {
+                category = categoryService.getCategoryByName(name);
+            }
+
+            if (category.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+}
